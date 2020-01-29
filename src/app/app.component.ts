4e6cf8da309e1acb3ae7e6a8f35971d73d40e680
DaniilYurin <emopot71@gmail.com>
2020-01-29 20:35:49 +0500

    table without thead

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-Lessons';
+  head = [['Номер','id']/* ,['ФИО','name'] */,['Имя','fName'],['Фамилия','lName'],['Отчество','middleName']];
+  people = [
+    {id:'1', fName:'Даниил', lName:'Юрин', middleName:'Андреевич'},
+    {id:'2', fName:'Александр', lName:'Олейников', middleName:'Витальевич'},
+    {id:'3', fName:'Сергей', lName:'Булыгин', middleName:'Евгеньевич'}
+  ];
 }
